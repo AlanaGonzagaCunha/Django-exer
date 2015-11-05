5b15c493f003feeae701d3851c82a05d317edd89
AlanaGonzagaCunha <alanagonzagac@gmail.com>
2015-11-05 12:36:43 -0300

    Crie o login, configurei com pag inicial, mas ainda faltar salvar no banco e redirecionar pags

--- contato/urls.py
+++ contato/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index),
+
+	url(r'^$', views.login),
+	url(r'^$', views.cadastro),
+
 ]
--- contato/views.py
+++ contato/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request,'contato/index.html',{})
+
+def login(request): 
+	return render(request, 'contato/login.html', {})
+
+def cadastro(request):
+	return render(request, 'contato/cadastro.html', {})
